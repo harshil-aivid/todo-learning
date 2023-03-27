547e4b987f6d7df2f83eeba663b02e293d758599
Janki Patel <janki.patel@dynacommerce.com>
2023-03-27 16:08:59 +0530

    add delete task and updated add button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 export default function Todo(props) {
-  const { title, status, toggleStatus } = props;
+  const { title, status, toggleStatus, handleRemove } = props;
 
   return (
     <div>
       <input type="checkbox" checked={status} onChange={toggleStatus} />
       {title}
+      <button type="button" onClick={handleRemove}>
+        Remove
+      </button>
     </div>
   );
 }
